aceb7a2de366c507bc5bf6688051d54f59b680f0
sakethkura <saketh.kura@gmail.com>
2020-02-04 09:45:50 -0500

    changed the mainActivity.java file to have onClick and buttons and displays
    made some strings in strings.xml
    
    add some textboxes to activity.xml

--- app/src/main/java/com/kurasaketh/quizapp/MainActivity.java
+++ app/src/main/java/com/kurasaketh/quizapp/MainActivity.java
@@ -2,12 +2,43 @@ package com.kurasaketh.quizapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button submitButton;
+    EditText responseText;
+    TextView displayText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitButton = findViewById(R.id.clickButton);
+        responseText = findViewById(R.id.responseEditText);
+        displayText = findViewById(R.id.textBox);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hello mom");
+                Log.i("testButton", "hi dad!");
+                Log.i("testButton", "Hello: " + responseText.getText().toString());
+                displayText.setText(displayText.getText().toString() + " " + responseText.getText().toString());
+            }
+        });
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    if(responseText.getText().toString().equals("TJ")){
+                        displayText.setText("TJ Rocks");
+                        responseText.setText("");
+                        responseText.setHint("That's a good name.");
+                    }
+                }
+            }
+        });
     }
 }
